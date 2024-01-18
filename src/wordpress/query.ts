96997716ef05bd95bc93a7b4379ec0965c92321d
Sazzad Hossain <tanimsazzad@gmail.com>
2024-01-18 16:51:45 +0600

    added try catch

--- src/wordpress/query.ts
+++ src/wordpress/query.ts
@@ -4,18 +4,22 @@ interface WPQUERY {
 }
 
 export const wpquery = async ({ query, variables = {} }: WPQUERY) => {
-  const response = await fetch("http://portfolioastrowordpress.local/graphql", {
-    headers: { "Content-Type": "application/json" },
-    method: "post",
-    body: JSON.stringify({ query, variables }),
-  });
+  try {
+    const response = await fetch(
+      "http://portfolioastrowordpress.local/graphql",
+      {
+        headers: { "Content-Type": "application/json" },
+        method: "post",
+        body: JSON.stringify({ query, variables }),
+      }
+    );
+    if (!response.ok) {
+      console.error(response);
+    }
 
-  if (!response.ok) {
-    console.error(response);
-    return {};
+    const { data } = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
   }
-
-  const { data } = await response.json();
-
-  return data;
 };
